f380a0b05ef3c5bd1159b24de685dc6755957190
Developer <dev@example.com>
2026-05-29 00:20:56 +0900

    구글 애드센스 승인 코드 삽입 완료

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8195982419600082"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body className="min-h-full flex flex-col">
         {children}
         <Analytics />
